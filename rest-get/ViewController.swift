d2cca57149dde1aa4b658944ede01cd85b998a64
Geoff Dudgeon <dudgeon@gmail.com>
2015-09-19 07:54:43 -0400

    1. Created basic interface (no connectivity) 2. Imported Alamofire Cocoapod

--- rest-get/ViewController.swift
+++ rest-get/ViewController.swift
@@ -9,6 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /// Outlets
+    @IBOutlet weak var restURI: UITextField!
+    @IBOutlet weak var restMessage: UITextView!
+    
+    
+    /// Actions
+    @IBAction func restGet(sender: AnyObject) {
+        
+        // Dismiss keyboard
+        restURI.resignFirstResponder()
+        
+        /// test basic functionality
+        restMessage.text = restURI.text
+        
+        
+        
+        
+
+        
+    }
+
+    
+    /// [_] Map return key to restGet()
+    
+    
+    
+    /////////////////////////////////////
+    /// Leaving this stuff alone for now:
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +48,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    ////////////////////////////////////
 
 }
 
